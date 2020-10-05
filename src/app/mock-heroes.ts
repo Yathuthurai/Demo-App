0f48ee3599bd37900eba1c6c406f39da5e0e9368
Yathuthurai <50237091+Yathuthurai@users.noreply.github.com>
2020-10-05 18:41:43 +0530

    First commit of demo app

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Abdul Kalaam' },
+  { id: 12, name: 'Sundar Pichai' },
+  { id: 13, name: 'Tony Stark' },
+  { id: 14, name: 'Harry Potter' },
+  { id: 15, name: 'Ronaldo' },
+  { id: 16, name: 'Mr Robot' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' },
+];
